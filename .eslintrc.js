1aac674dd57ace08cce6c59fd6345b5118d32c66
Ivan Artemiev <29709626+iartemiev@users.noreply.github.com>
2021-11-26 09:04:08 -0500

    fix: eslint for tests; remove coverage dir from git

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		tsconfigRootDir: __dirname,
-		project: ['./tsconfig.json'],
+		project: ['./tsconfig.json', './__tests__/tsconfig.jest.json'],
 	},
 	plugins: ['@typescript-eslint', 'jest'],
 	extends: [
